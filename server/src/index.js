bbe4e7725c5833e29f61920c54352f4aa6509dcf
lhall383 <lahall1009@aol.com>
2021-03-10 01:46:31 -0600

    Replace request with axios

--- server/src/index.js
+++ server/src/index.js
@@ -1,53 +1,71 @@
 const express = require("express");
-const request = require("request");
+const axios = require("axios");
+const qs = require('qs');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const getAuth = async () => {
+  try {
+    const response = await axios({
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      data: qs.stringify({
+        grant_type: 'client_credentials'
+      }),
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      auth: {
+        username: process.env.CLIENT_ID,
+        password: process.env.CLIENT_SECRET,
+      }
+    });
+
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getUserPublic = async (username, bearerToken) => {
+  try {
+    const response = await axios({
+      method: 'get',
+      url: 'https://api.spotify.com/v1/users/' + username,
+      headers: {
+        'Authorization': 'Bearer ' + bearerToken,
+        'content-type': 'application/x-www-form-urlencoded'
+      }
+    });
+
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 app.get("/test", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-app.get("/spotify-test", (req, res) => {
-  console.log('spotify test');
+app.get("/cc-auth-test", async (req, res) => {
+  console.log('client credentials auth test')
 
-  const authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: {
-      'Authorization': 'Basic ' + (new Buffer(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'))
-    },
-    form: {
-      grant_type: 'client_credentials'
-    },
-    json: true
-  };
+  const authData = await getAuth();
+  res.json(authData);
+});
 
-  request.post(authOptions, function(error, response, body) {
-    if (error) {
-      console.log('request failed')
-    }
+app.get("/user-public-test", async (req, res) => {
+  console.log('public user data test');
 
-    if (!error && response.statusCode === 200) {
-      // use the access token to access the Spotify Web API
-      console.log("Got auth token from spotify: " + body.access_token);
-      var token = body.access_token;
-      var options = {
-        url: 'https://api.spotify.com/v1/users/jmperezperez',
-        headers: {
-          'Authorization': 'Bearer ' + token
-        },
-        json: true
-      };
-      console.log("Making get request to spotify");
-      request.get(options, function(error, response, body) {
-        output = body;
-        console.log("Got valid response from spotify, display name: " + body.display_name);
-        res.json(body);
-      });
-    }
-  });
+  const authData = await getAuth();
+  const userData = await getUserPublic('jmperezperez', authData.access_token);
+  res.json(userData);
 });
 
 app.listen(PORT, () => {
