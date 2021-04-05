d945635db8673a4995ba363ef91181f54fbb4810
lhall383 <lahall1009@aol.com>
2021-04-05 01:52:22 -0500

    Configure CORS and reload on change.

--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
-const qs = require('qs');
-require('dotenv').config();
+const qs = require("qs");
+require("dotenv").config();
 
 const getAuth = async () => {
   try {
     const response = await axios({
-      method: 'post',
-      url: 'https://accounts.spotify.com/api/token',
+      method: "post",
+      url: "https://accounts.spotify.com/api/token",
       data: qs.stringify({
-        grant_type: 'client_credentials'
+        grant_type: "client_credentials",
       }),
       headers: {
-        'content-type': 'application/x-www-form-urlencoded'
+        "content-type": "application/x-www-form-urlencoded",
       },
       auth: {
         username: process.env.CLIENT_ID,
         password: process.env.CLIENT_SECRET,
-      }
+      },
     });
 
     console.log(response.data);
@@ -31,12 +31,12 @@ const getAuth = async () => {
 const getUserPublic = async (username, bearerToken) => {
   try {
     const response = await axios({
-      method: 'get',
-      url: 'https://api.spotify.com/v1/users/' + username,
+      method: "get",
+      url: "https://api.spotify.com/v1/users/" + username,
       headers: {
-        'Authorization': 'Bearer ' + bearerToken,
-        'content-type': 'application/x-www-form-urlencoded'
-      }
+        Authorization: "Bearer " + bearerToken,
+        "content-type": "application/x-www-form-urlencoded",
+      },
     });
 
     console.log(response.data);
@@ -44,30 +44,38 @@ const getUserPublic = async (username, bearerToken) => {
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.get("/test", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
 app.get("/cc-auth-test", async (req, res) => {
-  console.log('\n\nclient credentials auth test')
+  console.log("\n\nclient credentials auth test");
 
   const authData = await getAuth();
   res.json(authData);
 });
 
 app.get("/user-public", async (req, res) => {
-  console.log('\n\npublic user data for: ' + req.query.username);
+  console.log("\n\npublic user data for: " + req.query.username);
   console.log(req.query);
 
   const authData = await getAuth();
-  const userData = await getUserPublic(req.query.username, authData.access_token);
+  const userData = await getUserPublic(
+    req.query.username,
+    authData.access_token
+  );
   res.json(userData);
 });
 
