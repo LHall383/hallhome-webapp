e087b8a1f70d001ffa69afbf0f91e3272134cb0a
lhall383 <lahall1009@aol.com>
2021-04-29 18:23:50 -0500

    Add Luxon for dates and times

--- react-app/src/utils/utils.js
+++ react-app/src/utils/utils.js
@@ -1,3 +1,5 @@
+import { DateTime, Duration } from 'luxon';
+
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
@@ -27,36 +29,28 @@ export function toTitleCase(text) {
 
 /**
  * Format the duration of a song into min:sec i.e. 2:46
- * @param  {number} duration_ms The duration in milliseconds to conver
+ * @param  {number} durationMs The duration in milliseconds to conver
  * @return {string} The duration formatted in MM:SS form
  */
-export function durationToMinSec(duration_ms) {
-  const min = Math.floor(duration_ms / 1000 / 60);
-  const sec = Math.floor(((duration_ms / 1000 / 60) % 1) * 60);
-  return min + ':' + ('00' + sec).slice(-2);
+export function durationToMinSec(durationMs) {
+  const duration = Duration.fromMillis(durationMs);
+  return duration.toFormat('m:ss');
 }
 
 /**
- * Convert a date object to date time string
- * @param   {Date} date A date object to convert
- * @returns {string} A string containing the date and local time
+ * Convert ISO time string to date or time depending on range
+ * @param {string} isoString ISO string
+ * @returns {string} String containing time if ISO string is today, otherwise contains date string
  */
-export function dateToDateTimeString(date) {
-  const todays_date = new Date().toLocaleDateString('en-US');
-  if (date.toLocaleDateString('en-US') === todays_date) {
-    const isPM = date.getHours() >= 12;
-    var hours = isPM ? date.getHours() - 12 : date.getHours();
-    hours = hours === 0 ? 12 : hours;
+export function isoToTimeOrDate(isoString) {
+  const dt = DateTime.fromISO(isoString);
+  const now = DateTime.now();
 
-    return (
-      ('00' + hours).slice(-2) +
-      ':' +
-      ('00' + date.getMinutes()).slice(-2) +
-      ':' +
-      ('00' + date.getSeconds()).slice(-2) +
-      (isPM ? ' PM' : ' AM')
-    );
+  // If it is the same day, return a time string
+  if (dt.startOf('day').toMillis() === now.startOf('day').toMillis()) {
+    return dt.toLocaleString(DateTime.TIME_SIMPLE);
   }
 
-  return date.toISOString().slice(0, 10);
+  // Otherwise, return a date string
+  return dt.toLocaleString();
 }
